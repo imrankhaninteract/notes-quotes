f6b0935b4a033cfd15e62070206249985f9107e0
imrankhan <imrankhaninteract@gmail.com>
2024-02-23 14:48:11 +0600

    quoteChange

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 // Array of quotes
 const quotes = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-    "Believe you can and you're halfway there. -Theodore Roosevelt",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "The only impossible journey is the one you never begin. - Tony Robbins",
+    "The only way to do great work is to love what you do. - Steve Jobs, " +
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs, " +
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt, " +
+    "Your time is limited, don't waste it living someone else's life. - Steve Jobs, " +
+    "Believe you can and you're halfway there. - Theodore Roosevelt, " +
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt, " +
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill, " +
+    "The only impossible journey is the one you never begin. - Tony Robbins, " +
     "It always seems impossible until it's done. - Nelson Mandela"
 ];
 
